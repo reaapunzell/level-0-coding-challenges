25c31ff161afe9818dcba3644fc1104430c7f7c7
Rea Maroga <rea.maroga@gmail.com>
2023-05-12 13:49:53 +0200

    added task04.js

---
+++ task04.js
@@ -0,0 +1,10 @@
+function task04 () {
+function evenOrOdd(num){
+    if (num % 2 === 0){
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
+} 
+module.exports = { task04 };
