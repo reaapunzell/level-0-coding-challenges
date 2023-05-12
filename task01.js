5a9cde89f65938ddf199fec9fefeb5403f299052
Rea Maroga <rea.maroga@gmail.com>
2023-05-12 13:30:57 +0200

    added task01.js

---
+++ task01.js
@@ -0,0 +1,12 @@
+function task01(){
+    x = 0;
+    y = 1;
+    console.log(x);
+    console.log(y);
+    
+    x += 3;
+    y += x;
+    console.log(x);
+    console.log(y);
+};
+module.exports = { task01 };
