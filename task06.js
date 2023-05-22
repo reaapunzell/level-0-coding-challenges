f157081f71c91a2db4cc64c361b03856d18ba179
Rea Maroga <rea.maroga@gmail.com>
2023-05-22 11:00:55 +0200

    added task06

---
+++ task06.js
@@ -0,0 +1,10 @@
+function task06(a, b, c){
+    if (a > b && a > c){
+        return a ;
+      } else if (b > a && b > c){
+        return b ;
+      } else {
+        return c;
+      };
+    }
+ module.exports = { task06 } ;
